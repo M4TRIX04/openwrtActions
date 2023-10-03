6db3aaed55e2d5c7cfcbb2c5ee56f0a9cc23bcab
Have fun with your bytes <worldofliu@gmail.com>
2023-10-03 18:52:54 +0800

    Create diy-part3.sh

---
+++ diy-part3.sh
@@ -0,0 +1,2 @@
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git^a50cdb9" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git^f1b63af" >> "feeds.conf.default"
