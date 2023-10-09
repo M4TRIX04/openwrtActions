6a7f0f91f86901313656acca3428fc8e5505d827
Have fun with your bytes <worldofliu@gmail.com>
2023-10-09 18:21:42 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,2 +1,2 @@
-echo "src-git passwall_packages https://github.com/M4TRIX04/openwrt-passwall-packages.git" >> "feeds.conf.default"
-echo "src-git passwall https://github.com/M4TRIX04/openwrt-passwall.git" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default" \
+&& echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
