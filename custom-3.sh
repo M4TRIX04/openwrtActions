4d1c3ab05f9dfd8541cde430084804fff7331b87
Have fun with your bytes <worldofliu@gmail.com>
2023-10-09 18:25:40 +0800

    Update custom-3.sh
    Adding passwall repository

--- custom-3.sh
+++ custom-3.sh
@@ -1,2 +1,16 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: custom-2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Adding passwall repository
+
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default" \
 && echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
