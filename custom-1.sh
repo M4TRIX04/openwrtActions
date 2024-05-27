7fc13523c8d694fb58f74b37a1a6ece29c3f573e
Have fun with your bytes <worldofliu@gmail.com>
2024-05-27 16:54:31 +0800

    Update custom-1.sh

--- custom-1.sh
+++ custom-1.sh
@@ -14,5 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small.git^8dfade7192162fade6d05b6447da4f7fe6b8bbe6' >>feeds.conf.default
