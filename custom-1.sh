8e7e87d41b47bac4a30c6776f0df8b1a63d03d43
Have fun with your bytes <worldofliu@gmail.com>
2024-05-29 19:23:44 +0800

    Update custom-1.sh

--- custom-1.sh
+++ custom-1.sh
@@ -14,4 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git small https://github.com/kenzok8/small.git^8dfade7192162fade6d05b6447da4f7fe6b8bbe6' >>feeds.conf.default
+
+# git luci on the latest hysteria 1.3.5 commit
+sed -i 's/luci.git/luci.git^084b82067ce7e9664fbf471597acce613521044e/g' feeds.conf.default
