55fa5110fd3a61825e30878606bddfb51511ea60
Have fun with your bytes. <worldofliu@gmail.com>
2025-01-01 18:02:19 +0800

    Update and rename custom-3.sh to wr30u_hanwckf.sh

--- wr30u_hanwckf.sh
+++ wr30u_hanwckf.sh
@@ -12,5 +12,5 @@
 
 # Adding passwall repository
 
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default" \
-&& echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default" \
+#&& echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
